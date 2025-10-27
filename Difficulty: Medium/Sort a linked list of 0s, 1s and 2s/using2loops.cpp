28756403adb5b05e8cddd1eb40389d4b6fb52fc0
Anupam Karmakar <anupamkarmakar27@gmail.com>
2025-10-27 12:57:53 +0530

    TC : 2n

---
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/using2loops.cpp
@@ -0,0 +1,45 @@
+/* Node is defined as
+  class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        // code here
+        int count0=0, count1=0, count2=0;
+        Node *p=head;
+        
+        while(p){
+            if(p->data==0) count0++;
+            else if(p->data==1) count1++;
+            else count2++;
+            p=p->next;
+        }
+        
+        p=head;
+        while(p){
+            if(count0){
+                p->data=0;
+                count0--;
+            }
+            else if(count1){
+                p->data=1;
+                count1--;
+            }
+            else if(count2){
+                p->data=2;
+                count2--;
+            }
+            p=p->next;
+        }
+        return head;
+    }
+};
