67fc994390adfd14ecf1386d40b929202af26550
Anupam Karmakar <anupamkarmakar27@gmail.com>
2026-04-01 16:23:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Consecutive 1's not allowed/consecutive-1s-not-allowed.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countStrings(int n) {
+        if (n == 1) return 2;
+        
+        int prev2 = 2; // dp[1]
+        int prev1 = 3; // dp[2]
+        
+        for (int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        
+        return prev1;
+    }
+};
